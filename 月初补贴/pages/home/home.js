cd2b679e7bbfc6f2cce1e467e40ba768e019161c
liruidesysu <li_read@outlook.com>
2019-01-03 21:51:56 +0800

    添加主页搜索框

--- 月初补贴/pages/home/home.js
+++ 月初补贴/pages/home/home.js
@@ -1,3 +1,7 @@
+//获取应用实例
+var WxSearch = require('../../wxSearch/wxSearch.js')
+var app = getApp()
+
 Page({
   data: {
     currentCity: ''
@@ -20,7 +24,7 @@ Page({
   loadCity: function (longitude, latitude) {
     var page = this
     wx.request({
-      url: 'https://api.map.baidu.com/geocoder/v2/?ak=您的ak&location=' + latitude + ',' + longitude + '&output=json',
+      url: 'https://api.map.baidu.com/geocoder/v2/?ak=zKM11815YKDzcU9dFsFH6lqUqVquRL9e&location=' + latitude + ',' + longitude + '&output=json',
       data: {},
       header: {
         'Content-Type': 'application/json'
@@ -32,8 +36,60 @@ Page({
         page.setData({ currentCity: city });
       },
       fail: function () {
-        page.setData({ currentCity: "获取定位失败" });
+        page.setData({ currentCity: "未知" });
       },
     })
+  },
+  inputSearch: function (e) {  //输入框根据查询条件搜索点击事件
+    // 获取用户输入框中的值
+    let inputVaue = e.detail.value['search-input'] ? e.detail.value['search-input'] : e.detail.value;
+    if (!inputVaue) {
+      throw new Error('search input contents con not empty!');
+      return;
+    }
+    let searchUrl = "/product/index?keyword=" + inputVaue + "&fromindex=true";
+    this.converterUrlAndRedirect(searchUrl);
+  },
+  // 搜索框
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //初始化的时候渲染wxSearchdata
+    WxSearch.init(that, 43, ['weappdev', '小程序', 'wxParse', 'wxSearch', 'wxNotification']);
+    WxSearch.initMindKeys(['weappdev.com', '微信小程序开发', '微信开发', '微信小程序']);
+  },
+  wxSearchFn: function (e) {
+    var that = this
+    WxSearch.wxSearchAddHisKey(that);
+
+  },
+  wxSearchInput: function (e) {
+    var that = this
+    WxSearch.wxSearchInput(e, that);
+  },
+  wxSerchFocus: function (e) {
+    var that = this
+    WxSearch.wxSearchFocus(e, that);
+  },
+  wxSearchBlur: function (e) {
+    var that = this
+    WxSearch.wxSearchBlur(e, that);
+  },
+  wxSearchKeyTap: function (e) {
+    var that = this
+    WxSearch.wxSearchKeyTap(e, that);
+  },
+  wxSearchDeleteKey: function (e) {
+    var that = this
+    WxSearch.wxSearchDeleteKey(e, that);
+  },
+  wxSearchDeleteAll: function (e) {
+    var that = this;
+    WxSearch.wxSearchDeleteAll(that);
+  },
+  wxSearchTap: function (e) {
+    var that = this
+    WxSearch.wxSearchHiddenPancel(that);
   }
+
 })  

09e29970838ee25d21e40226226e5df47320b0b1
liruidesysu <li_read@outlook.com>
2019-01-04 12:38:32 +0800

    扫码页面添加

--- 月初补贴/pages/index/index.js
+++ 月初补贴/pages/index/index.js
@@ -14,9 +14,34 @@ Page({
     console.log('onLoad')
     var that = this
     //初始化的时候渲染wxSearchdata
-    WxSearch.init(that,43,['weappdev','小程序','wxParse','wxSearch','wxNotification']);
+    WxSearch.init(that,43,['一点点','杨国福','牛大学','炉火鱼香','泰莱']);
     WxSearch.initMindKeys(['weappdev.com','微信小程序开发','微信开发','微信小程序']);
   },
+
+  // 扫码点餐
+  scan2restaurant: function(){
+    //wx.navigateTo({
+      //url: '../scan/scan',
+    //})
+    wx.scanCode({
+      onlyFromCamera: false,
+      scanType: ['qrCode', 'barCode'],
+      success: function (suc) {
+        wx.navigateTo({
+          url: '../order-food/order-food',
+          success: function (res) { },
+          fail: function (res) { },
+          complete: function (res) { },
+        })
+      },
+      fail: function (err) {
+      },
+      complete: function (com) {
+      }
+    })
+  },
+
+  // 搜索框
   wxSearchFn: function(e){
     var that = this
     WxSearch.wxSearchAddHisKey(that);

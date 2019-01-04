5b0f4d33f05b6a03d3ac9ac26ce94b16a29d19c4
xiongdajq <wangjiaqi1995@163.com>
2019-01-04 16:27:33 +0800

    get user information

--- 月初补贴/pages/personal/order.js
+++ 月初补贴/pages/personal/order.js
@@ -7,14 +7,32 @@ Page({
   data: {
     userInfo: {},
     history_order:"历史订单",
-    login:"登录/注册",
+    login:"登录",
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+  },
+  loginandup:function(e) {
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.getSetting({
+      success(res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success(res) {
+              console.log(res.userInfo)
+            }
+          })
+        }
+      }
+    })
+  },
+  bindGetUserInfo(e) {
+    console.log(e.detail.userInfo)
   },
 
   /**
